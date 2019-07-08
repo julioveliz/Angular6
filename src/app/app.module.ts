973dc66d48367188d94d621729258799046bd257
julio veliz <julio.cesar.veliz.s@gmail.com>
2019-07-08 19:00:17 -0400

    Primer Componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorld } from './primercomponente/primer.component';
 
-@NgModule({
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorld
   ],
   imports: [
     BrowserModule
