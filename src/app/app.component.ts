4247da32bf2c75db284627703daa9d4c874e3588
julio veliz <julio.cesar.veliz.s@gmail.com>
2019-07-08 21:23:24 -0400

    Borrar y Agregar Usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+  //title = 'myfirstapp';
+  
+  users = ['rayn','joe','rick','bruce','jhon','mary'];
+  
+  sayHello(){
+    alert('Hello');
+  }
+
+  DeleteUser(user){
+    
+    for(let i=0; i < this.users.length; i++){
+        if (user==this.users[i]){
+          this.users.splice(i,1);
+        }
+    }
+  }
+
+  AddUser(newUser){
+    //console.log(newUser.value);
+    this.users.push(newUser.value);
+    newUser.value='';
+    newUser.focus();
+    return false;
+    
+    for(let i=0; i < this.users.length; i++){
+        if (newUser==this.users[i]){
+          //this.users.splice(i,1);
+        }
+    }
+  }
+  //activated: boolean =true;
+  /*
+
+  name :string = 'julio veliz';
+  age : number;
+  address: {
+    street: string;
+    city:string;
+  };
+  hobbies : string[] ;
+
+  constructor(){
+    this.age=49;
+    this.address = {
+      street:'calle 39 entre 18 y 19',
+      city:'Bqto'
+    }
+  this.hobbies = ['leer','cantar','jugar','orar']; 
+
+  }
+  */
+  //hobbies = ['leer','cantar','jugar','orar']
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloWorld } from './primercomponente/primer.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({ 
   declarations: [
     AppComponent,
-    HelloWorld
+    HolaMundoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
